18b718c2ad749ae982f330f1cbed1d7a267d046f
David R. MacIver <david@drmaciver.com>
2013-02-13 12:38:53 +0000

    switch over to parameterized test

--- majority_judgement_test.py
+++ majority_judgement_test.py
@@ -1,66 +1,65 @@
 from majority_judgement import MajorityJudgement
+import pytest
 
 class TestMajorityJudgementSoundness:
-    def test_tallies(self):
-      def do_test(x):
-        tallies = [0 for _ in x]
-        for i in MajorityJudgement(x): tallies[i] = tallies[i] + 1
-        assert x == tallies
+    example_votes = [
+      [1,2,3],
+      [13],
+      [1, 13, 12, 11, 1],
+      [1, 1, 1, 1],
+      [2,3],
+      [2,1,3],
+      [10,1,10],
+      [1,3,2],
+      [10], 
+      [9],
+      [10], 
+      [9, 1]
+    ]
 
-      do_test([1,2,3])
-      do_test([13])
-      do_test([1, 13, 12, 11, 1])
+    on_all_examples = pytest.mark.parametrize("x", example_votes)
+
+    @on_all_examples
+    def test_tallies(self, x):
+      tallies = [0 for _ in x]
+      for i in MajorityJudgement(x): tallies[i] = tallies[i] + 1
+      assert x == tallies
 
 
-    def test_not_less_than_self(self):
-      def do_test(x):
-        assert MajorityJudgement(x) >= MajorityJudgement(x)
+    @on_all_examples
+    def test_not_less_than_self(self, x):
+      assert MajorityJudgement(x) >= MajorityJudgement(x)
 
-      do_test([1,2,3])
-      do_test([10])
-      do_test([10, 1, 10])
+    @on_all_examples
+    def test_front_loading_moves_earlier(self, x):
+      for i in xrange(1, len(x)):
+        y = list(x)
+        y[i] = y[i] - 1
+        y[i-1] = y[i-1] + 1
+        assert MajorityJudgement(y) < MajorityJudgement(x)
 
-    def test_front_loading_moves_earlier(self):
-      def do_test(x):
-        for i in xrange(1, len(x)):
-          y = list(x)
-          y[i] = y[i] - 1
-          y[i-1] = y[i-1] + 1
-          assert MajorityJudgement(y) < MajorityJudgement(x)
-
-      do_test([1, 1, 1, 1])
-      do_test([2,3])
-      do_test([2,1,3])
-      do_test([10,1,10])
-
-    def test_equality_of_same_input(self):
-      x = MajorityJudgement([1,2,3])
+    @on_all_examples
+    def test_equality_of_same_input(self, x):
+      x = MajorityJudgement(x)
       assert x == x
 
-    def test_equality_of_identical_inputs(self):
-      def do_test(x): assert MajorityJudgement(x) == MajorityJudgement(x)
+    @on_all_examples
+    def test_equality_of_identical_inputs(self, x):
+      assert MajorityJudgement(x) == MajorityJudgement(x)
 
-      do_test([1,2,3])
-      do_test([10, 1, 1, 1,])
-      do_test([10, 1, 1, 8])
+    @pytest.mark.parametrize(("x", "y"), [
+      (x, y) for x in example_votes for y in example_votes if x != y 
+    ])
+    def test_inequality_of_different_inputs(self, x, y):
+      assert MajorityJudgement(x) != MajorityJudgement(y)
 
-    def test_inequality_of_different_inputs(self):
-      def do_test(x, y): assert MajorityJudgement(x) != MajorityJudgement(y)
-
-      do_test([1,2,3], [1,3,2])
-      do_test([10], [9])
-      do_test([10], [9, 1])
-
-    def test_correct_number(self):
-      def correct_number_test(x):
-        total = sum(x)
-        assert len(list(MajorityJudgement(x))) == total
-      
-      correct_number_test([1,1,1,1])
-      correct_number_test([1,7,1,1])
-      correct_number_test([10])
-
-    def test_index_matches_list_index(self):
+    @on_all_examples
+    def test_correct_number(self, x):
+      total = sum(x)
+      assert len(list(MajorityJudgement(x))) == total
+     
+    @on_all_examples 
+    def test_index_matches_list_index(self, x):
       def index_test(x):
         index_result = []
         for i in xrange(0, sum(x)): index_result.append(MajorityJudgement(x)[i])
@@ -72,11 +71,11 @@ class TestMajorityJudgementSoundness:
       index_test([1, 2, 3])
       index_test([1,6,1])
 
-    def test_starts_with_median(self):
-      def median_test(x, m):
-        assert MajorityJudgement(x)[0] == m
-
-      median_test([1,1,1,1], 1)
-      median_test([5,1,1,5], 1)
-      median_test([5,1,1,6], 2)
-      median_test([1,1,1,1,20], 4)
+    @pytest.mark.parametrize(("x", "m"), [
+      ([1,1,1,1], 1),
+      ([5,1,1,5], 1),
+      ([5,1,1,6], 2),
+      ([1,1,1,1,20], 4)
+    ])
+    def test_starts_with_median(self, x, m):
+      assert MajorityJudgement(x)[0] == m
