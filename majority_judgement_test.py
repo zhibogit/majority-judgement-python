f21b751f456aa546e2bc6d85e5fa9f05b67d0d56
David R. MacIver <david@drmaciver.com>
2013-02-13 12:21:05 +0000

    switch over to pytest

--- majority_judgement_test.py
+++ majority_judgement_test.py
@@ -1,7 +1,6 @@
-import unittest
 from majority_judgement import MajorityJudgement
 
-class TestMajorityJudgementSoundness(unittest.TestCase):
+class TestMajorityJudgementSoundness:
     def setUp(self):
       pass
 
@@ -9,7 +8,7 @@ class TestMajorityJudgementSoundness(unittest.TestCase):
       def do_test(x):
         tallies = [0 for _ in x]
         for i in MajorityJudgement(x): tallies[i] = tallies[i] + 1
-        self.assertEqual(x, tallies)
+        assert x == tallies
 
       do_test([1,2,3])
       do_test([13])
@@ -18,7 +17,7 @@ class TestMajorityJudgementSoundness(unittest.TestCase):
 
     def test_not_less_than_self(self):
       def do_test(x):
-        self.assertGreaterEqual(MajorityJudgement(x), MajorityJudgement(x))
+        assert MajorityJudgement(x) >= MajorityJudgement(x)
 
       do_test([1,2,3])
       do_test([10])
@@ -30,7 +29,7 @@ class TestMajorityJudgementSoundness(unittest.TestCase):
           y = list(x)
           y[i] = y[i] - 1
           y[i-1] = y[i-1] + 1
-          self.assertLess(MajorityJudgement(y), MajorityJudgement(x))
+          assert MajorityJudgement(y) < MajorityJudgement(x)
 
       do_test([1, 1, 1, 1])
       do_test([2,3])
@@ -39,17 +38,17 @@ class TestMajorityJudgementSoundness(unittest.TestCase):
 
     def test_equality_of_same_input(self):
       x = MajorityJudgement([1,2,3])
-      self.assertEqual(x, x)
+      assert x == x
 
     def test_equality_of_identical_inputs(self):
-      def do_test(x): self.assertEqual(MajorityJudgement(x), MajorityJudgement(x))
+      def do_test(x): assert MajorityJudgement(x) == MajorityJudgement(x)
 
       do_test([1,2,3])
       do_test([10, 1, 1, 1,])
       do_test([10, 1, 1, 8])
 
     def test_inequality_of_different_inputs(self):
-      def do_test(x, y): self.assertNotEqual(MajorityJudgement(x), MajorityJudgement(y))
+      def do_test(x, y): assert MajorityJudgement(x) != MajorityJudgement(y)
 
       do_test([1,2,3], [1,3,2])
       do_test([10], [9])
@@ -58,7 +57,7 @@ class TestMajorityJudgementSoundness(unittest.TestCase):
     def test_correct_number(self):
       def correct_number_test(x):
         total = sum(x)
-        self.assertEqual(len(list(MajorityJudgement(x))), total)
+        assert len(list(MajorityJudgement(x))) == total
       
       correct_number_test([1,1,1,1])
       correct_number_test([1,7,1,1])
@@ -70,7 +69,7 @@ class TestMajorityJudgementSoundness(unittest.TestCase):
         for i in xrange(0, sum(x)): index_result.append(MajorityJudgement(x)[i])
 
         list_result = list(MajorityJudgement(x))
-        self.assertEqual(index_result, list_result)
+        assert index_result == list_result
 
       index_test([10])
       index_test([1, 2, 3])
@@ -78,13 +77,9 @@ class TestMajorityJudgementSoundness(unittest.TestCase):
 
     def test_starts_with_median(self):
       def median_test(x, m):
-        self.assertEqual(MajorityJudgement(x)[0], m)
+        assert MajorityJudgement(x)[0] == m
 
       median_test([1,1,1,1], 1)
       median_test([5,1,1,5], 1)
       median_test([5,1,1,6], 2)
       median_test([1,1,1,1,20], 4)
-
-
-if __name__ == '__main__':
-    unittest.main()
