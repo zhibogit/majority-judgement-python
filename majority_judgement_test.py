2bb9c4acd1d5f582554d11d00e8df589254d1235
David R. MacIver <david@drmaciver.com>
2013-02-17 14:32:51 +0000

    indentation

--- majority_judgement_test.py
+++ majority_judgement_test.py
@@ -217,17 +217,19 @@ class TestMajorityJudgementSoundness:
             assert MajorityJudgement([1]) > MajorityJudgement([])
 
         def test_no_delete(self):
-          x = MajorityJudgement([1,2,3])
-          l = list(x)
-          with pytest.raises(TypeError) as excinfo:
-              del x[0]
-          assert re.search('not support modifying', excinfo.value.message)
-          assert list(x) == l
+            x = MajorityJudgement([1,2,3])
+            l = list(x)
+            with pytest.raises(TypeError) as excinfo:
+                del x[0]
+            assert re.search('not support modifying', excinfo.value.message)
+            assert list(x) == l
 
         def test_no_set(self):
-          x = MajorityJudgement([1,2,3])
-          l = list(x)
-          with pytest.raises(TypeError) as excinfo:
-              x[0] = 15
-          assert re.search('not support modifying', excinfo.value.message)
-          assert list(x) == l
+            x = MajorityJudgement([1,2,3])
+            l = list(x)
+            with pytest.raises(TypeError) as excinfo:
+                x[0] = 15
+            assert re.search('not support modifying', excinfo.value.message)
+            assert list(x) == l
+
+        
