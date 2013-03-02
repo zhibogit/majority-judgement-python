793cf3e48673bcd4a79843a6a489c5b1aaad9798
David R. MacIver <david@drmaciver.com>
2013-03-02 17:35:13 +0000

    version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ class PyTest(TestCommand):
 
 setup(
     name='majorityjudgement',
-    version='0.2.0',
+    version='0.3.0',
     author='David R. MacIver',
     author_email='david@drmaciver.com',
     packages=['majorityjudgement'],
